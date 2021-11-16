7e4d204537ed1255f763e765f6dc4c17b128bada
Mljungho <mathias.ljungholm@gmail.com>
2021-11-16 15:26:10 +0100

    Finished chapter regarding Styling and Content

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <Segment inverted>
+      <Menu inverted secondary>
+        <Menu.Item id="header" name="my portfolio"/>
+      </Menu>
+    </Segment>
+  );
+};
+
+export default Header;
---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+import { Container } from "semantic-ui-react";
+
+const Hello = () => {
+  return (
+    <Container>
+      <h1 id="hello">Hello World</h1>
+    </Container>
+  );
+};
+
+export default Hello;
