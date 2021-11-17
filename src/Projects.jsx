f72c8c559acd056db163a7bef61d2a07a3eed16b
Mljungho <mathias.ljungholm@gmail.com>
2021-11-17 12:42:59 +0100

    adding unit test for projects. in projects.jsx: Changed state from a stateless function to statefull class seting initial state for projects by ID and name key.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
 
-const Projects = () => {
-  return (
-    <Container>
-      <h1 id="projects-header">My Projects</h1>
-      <p>some cool text about my project</p>
-    </Container>
-  );
-};
+class Projects extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        name: "My First Website",
+      },
+      {
+        id: 2,
+        name: "Fizzbuzz",
+      },
+    ],
+  };
+
+  render() {
+    const { Projects } = this.state;
+
+    let projectsList = projects.map((project) => {
+      return (
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+
+    return (
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
+    );
+  }
+}
 
 export default Projects;
