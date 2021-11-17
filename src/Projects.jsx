a9e94fc78f2672f7cff5614e7e87172cc7db32b4
Mljungho <mathias.ljungholm@gmail.com>
2021-11-17 14:35:00 +0100

    added card functionallity. Git saving prior to test passing. Want to be able to make a comparison before test pass and after.

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react';
+import { Container, Grid } from 'semantic-ui-react';
 import axios from 'axios';
+import ProjectCard from './ProjectCard'
 
 class Projects extends Component {
   state = {
@@ -21,7 +22,7 @@ class Projects extends Component {
     return (
       <Container>
         <h1 id="projects-header">My Projects</h1>
-        {projectsList}
+        <Grid>{projectsList}</Grid>
       </Container>
     );
   };
