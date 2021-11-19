dfd9999a03c4ca858296484612e33ce0b642b717
Mljungho <mathias.ljungholm@gmail.com>
2021-11-19 16:35:21 +0100

    stylized the cards, added comments & images

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,43 @@ const About = () => {
   return (
     <Container>
       <h1 id="about-header">About Me</h1>
-      <p>some cool text about me</p>
+
+      <h2 id="about-headline_1">Personal</h2>
+      <p>
+        I was born curious and in a long line of engineers that I was determined to break in my rebellious teenage years. 
+        And that didn't happen, things rarely end up exactly the way we expect. I found that Engineering tickled my curiosity in just the right way, There's always 
+        something to build or break, boundries to challenge and fidgets to figure out how they work. I have a love for understanding systems and how different component
+        coexist to create something greater than the sum of their own parts.
+
+        So it's a curious decition, some would say, for me to leave what I'm love doing, and enter the realm of the immaterial. 
+        That may be the case, but I want to know more. 
+        I love for puzzles, systems, and the many ways to break them. 
+
+        I love quotes from wise people throughout the course of history, and I believe there's so much wisdome that has faded away from contemporary knowladge.
+
+        I am undergoing this transformation from one sector to another, I like to say that I'm "going soft as I get older" - yes pun intended.
+      </p>
+
+      <h2 id="about-headline_2">Business life</h2>
+      <p>
+        I mentioned parts of my profession in the private category, the reason I did that is because a large part of who I am, is what I do.
+        as Marcus Aurelius said: You are what you do, repetedly" and I am in all intents and purpouse, a very curious individual!
+
+        Just after graduation at the end of 2012 i found a job as a Fuselage Engineer for Aaab Aeronautics in Linköping - Sweden. 
+        Worked there for two years untill I applied for a job as a Mechanical Integration Engineer (MIE / Package Engineer) in CEVT Göteborg.
+        I didn't know it at the time, but this was the massive segway into both system engineering and automotive that would consume me for the 
+        remaining chareer in hardware. Although I have worked for other car compannies such as Volvo Cars(VCC), as well as Aston Martin Lagonda (AML).
+        As a system engineer you are tasked with the oversight of balancing the myriad of requirements and needs that every single component needs in order
+        to function correctly. This role, I think, trained me in understanding the larger picture while still paying attention to detailed requirements.
+
+        I have been engaged in numerus taskforces that required extra attention and creative solutions, and I like to think that what I bring to the table 
+        in situations like that, is to see things differently, and callenge the status quo. 
+
+        I thrive when there's a challenge before me.
+      </p>
+
+
+      
     </Container>
   );
 };
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -3,7 +3,7 @@ import { Container } from "semantic-ui-react";
 const Hello = () => {
   return (
     <Container>
-      <h1 id="hello">Hello World</h1>
+      <h1 id="hello">Hello World, this is my portfolio</h1>
     </Container>
   );
 };
