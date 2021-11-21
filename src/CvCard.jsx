786c5f3322b503ceae4e41153f9eb53b55ad6f56
Mljungho <mathias.ljungholm@gmail.com>
2021-11-21 23:47:07 +0100

    refactored away some commented out code in CvCard

--- src/CvCard.jsx
+++ src/CvCard.jsx
@@ -3,17 +3,6 @@ import { Item, Image } from "semantic-ui-react";
 
 const CvCard = ({ cv }) => {
     return (
-        // <Card>
-        //     <Image src={cv.image} wrapped ui={false} />
-        //     <Card.Content>
-        //         <Card.Header>{cv.name}</Card.Header>
-        //         <Card.Header>{cv.prevwork}</Card.Header>
-        //         <Card.Meta>{cv.label}</Card.Meta>
-        //         <Card.Description>{cv.email}</Card.Description>
-        //         <Card.Description>{cv.phone}</Card.Description>
-        //         <Card.Description>{cv.summary}</Card.Description>
-        //     </Card.Content>
-        // </Card>
 
         <Item>
         <Item.Image size='small' src={cv.image} />
@@ -28,8 +17,6 @@ const CvCard = ({ cv }) => {
             <Item.Extra>{cv.summary}</Item.Extra>
         </Item.Content>
         </Item>
-
-
 
     );
 };
