4d1c709a831ba537137521d39fe00acaf10f0f1d
Mljungho <mathias.ljungholm@gmail.com>
2021-11-21 17:55:17 +0100

    changed CV layout from Card.content to Item.content

--- src/CvCard.jsx
+++ src/CvCard.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Item, Image } from "semantic-ui-react";
 
 const CvCard = ({ cv }) => {
     return (
-        <Card>
-            <Image src={cv.image} wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>{cv.name}</Card.Header>
-                <Card.Header>{cv.prevwork}</Card.Header>
-                <Card.Meta>{cv.label}</Card.Meta>
-                <Card.Description>{cv.email}</Card.Description>
-                <Card.Description>{cv.phone}</Card.Description>
-                <Card.Description>{cv.summary}</Card.Description>
-            </Card.Content>
-        </Card>
+        // <Card>
+        //     <Image src={cv.image} wrapped ui={false} />
+        //     <Card.Content>
+        //         <Card.Header>{cv.name}</Card.Header>
+        //         <Card.Header>{cv.prevwork}</Card.Header>
+        //         <Card.Meta>{cv.label}</Card.Meta>
+        //         <Card.Description>{cv.email}</Card.Description>
+        //         <Card.Description>{cv.phone}</Card.Description>
+        //         <Card.Description>{cv.summary}</Card.Description>
+        //     </Card.Content>
+        // </Card>
+
+        <Item>
+        <Item.Image size='small' src={cv.image} />
+
+        <Item.Content>
+            <Item.Header as='a'>{cv.name}</Item.Header>
+            <Item.Header as='a'>{cv.prevwork}</Item.Header>
+            <Item.Meta>{cv.label}</Item.Meta>
+            <Item.Description>
+            </Item.Description>
+            <Item.Extra>{cv.email}</Item.Extra>
+        </Item.Content>
+        </Item>
+
+
+
     );
 };
 
